3c38c2296e0cc9c90cc45fff84313805b0dc10da
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-08-12 13:25:53 +0530

    Tangent: Increase max site width

--- src/components/MaxWidthWrapper/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper/MaxWidthWrapper.tsx
@@ -3,7 +3,7 @@
 import {css} from "styled-components";
 
 const MaxWidthWrapper = css`
-  max-width: 1440px;
+  max-width: 1800px;
   padding-left: 32px;
   padding-right: 32px;
   margin-left: auto;
